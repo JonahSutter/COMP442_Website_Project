7b817e053d4f2d0c06fc7a36c4ef4ff288c1201d
Tyler Keyes <tekeyes@icloud.com>
2020-11-16 21:11:51 -0500

    Added routes for each page

--- server.py
+++ server.py
@@ -6,3 +6,19 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/login/")
 def login():
     return render_template("login.html")
+
+@app.route("/main/")
+def main():
+    return render_template("main_page.html")
+
+@app.route("/game/")
+def game():
+    return render_template("game_page.html")
+
+@app.route("/edit/")
+def edit():
+    return render_template("edit_account.html")
+
+@app.route("/create/")
+def create():
+    return render_template("create_account.html")
